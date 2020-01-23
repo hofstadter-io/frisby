5c915b715e5e6aefdd968bf7f0eec3e9f728685d
Raymond Rusk <raymond.rusk@educationplannerbc.ca>
2020-01-23 11:28:30 -0800

    Code cleanup

--- example/route_tester.go
+++ example/route_tester.go
@@ -12,33 +12,36 @@ import (
 	"github.com/EducationPlannerBC/frisby"
 )
 
+func getKeyVal(header string) (string, string) {
+	var key, val string
+	if header != "" {
+		res := strings.Split(header, ":")
+		if len(res) == 2 {
+			key = res[0]
+			val = res[1]
+		}
+	}
+	return key, val
+}
+
 func getMethod(testname string, url string, header1 string, header2 string, header3 string, header4 string, status string, expected string) {
-	F := frisby.Create(testname).
-		Get(url)
-	var res []string
-	if header1 != "" {
-		res = strings.Split(header1, ":")
+	F := frisby.Create(testname).Get(url)
+	var key, val string
+	key, val = getKeyVal(header1)
+	if key != "" {
+		F.SetHeader(key, val)
 	}
-	if len(res) == 2 {
-		F.SetHeader(res[0], res[1])
+	key, val = getKeyVal(header2)
+	if key != "" {
+		F.SetHeader(key, val)
 	}
-	if header2 != "" {
-		res = strings.Split(header2, ":")
+	key, val = getKeyVal(header3)
+	if key != "" {
+		F.SetHeader(key, val)
 	}
-	if len(res) == 2 {
-		F.SetHeader(res[0], res[1])
-	}
-	if header3 != "" {
-		res = strings.Split(header3, ":")
-	}
-	if len(res) == 2 {
-		F.SetHeader(res[0], res[1])
-	}
-	if header4 != "" {
-		res = strings.Split(header4, ":")
-	}
-	if len(res) == 2 {
-		F.SetHeader(res[0], res[1])
+	key, val = getKeyVal(header4)
+	if key != "" {
+		F.SetHeader(key, val)
 	}
 	i, err := strconv.Atoi(status)
 	if err != nil {
@@ -53,26 +56,19 @@ func getMethod(testname string, url string, header1 string, header2 string, head
 }
 
 func postMethod(testname string, url string, header1 string, header2 string, header3 string, jsondata string, status string, expected string) {
-	F := frisby.Create(testname).
-		Post(url)
-	var res []string
-	if header1 != "" {
-		res = strings.Split(header1, ":")
+	F := frisby.Create(testname).Post(url)
+	var key, val string
+	key, val = getKeyVal(header1)
+	if key != "" {
+		F.SetHeader(key, val)
 	}
-	if len(res) == 2 {
-		F.SetHeader(res[0], res[1])
+	key, val = getKeyVal(header2)
+	if key != "" {
+		F.SetHeader(key, val)
 	}
-	if header2 != "" {
-		res = strings.Split(header2, ":")
-	}
-	if len(res) == 2 {
-		F.SetHeader(res[0], res[1])
-	}
-	if header3 != "" {
-		res = strings.Split(header3, ":")
-	}
-	if len(res) == 2 {
-		F.SetHeader(res[0], res[1])
+	key, val = getKeyVal(header3)
+	if key != "" {
+		F.SetHeader(key, val)
 	}
 	if jsondata != "" {
 		F.SetJSON(jsondata)
@@ -93,32 +89,25 @@ func checkRoute(record []string) {
 		fmt.Println("The format of the csv file is invalid!")
 		os.Exit(1)
 	}
-
 	testname := record[0]
 	method := record[1]
 	url := record[2]
-
+	header1 := record[3]
+	header2 := record[4]
+	header3 := record[5]
+	status := record[7]
+	expected := record[8]
 	if method == "GET" {
-		header1 := record[3]
-		header2 := record[4]
-		header3 := record[5]
 		header4 := record[6]
-		status := record[7]
-		expected := record[8]
 		getMethod(testname, url, header1, header2, header3, header4, status, expected)
 	}
 	if method == "POST" {
-		header1 := record[3]
-		header2 := record[4]
-		header3 := record[5]
 		jsondata := record[6]
-		status := record[7]
-		expected := record[8]
 		postMethod(testname, url, header1, header2, header3, jsondata, status, expected)
 	}
 }
 
-func iterateroutes(routesfile string) {
+func iterateRoutes(routesfile string) {
 
 	// Open file describing expected routes
 	csvfile, err := os.Open(routesfile)
@@ -126,7 +115,7 @@ func iterateroutes(routesfile string) {
 		log.Fatalln("Couldn't open the csv file "+routesfile, err)
 	}
 
-	// Parse the file
+	// Parse the file using '|' as default separator and ignoring double-quotes
 	r := csv.NewReader(csvfile)
 	r.Comma = '|'
 	r.LazyQuotes = true
@@ -150,12 +139,13 @@ func iterateroutes(routesfile string) {
 func main() {
 
 	args := os.Args
-	len := len(args)
-	if len != 2 {
+	argsLen := len(args)
+	if argsLen != 2 {
 		fmt.Println("Usage: " + args[0] + " <routes file>.csv")
 		fmt.Println("\nThe routes file is a delimited text file that uses | to separate values")
 		fmt.Println("\nThe format is:")
-		fmt.Println("testName|method(e.g. GET)|url|header1|header2|header3|header4|responseStatus|expectedOutput")
+		fmt.Println("testName|GET|url|header1|header2|header3|header4|responseStatus|expectedOutput")
+		fmt.Println("testName|POST|url|header1|header2|header3|jsondata|responseStatus|expectedOutput")
 		fmt.Println("\nThe file route_file.csv illustrates the correct format.")
 		os.Exit(1)
 	}
@@ -164,7 +154,7 @@ func main() {
 
 	fmt.Println("Route Testing!")
 
-	iterateroutes(routesFile)
+	iterateRoutes(routesFile)
 
 	frisby.Global.PrintReport()
 }
